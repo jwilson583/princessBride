596f796ffd6ec9443c75127c954a412049116c6d
edwinyam <edwinyam@edwins-mbp.edzone>
2017-01-26 20:37:20 -0500

    Merge origin/master
    Conflicts:
    	princessBride/src/princessbride/PrincessBride.java

--- PrincessBride/src/princessbride/PrincessBride.java
+++ PrincessBride/src/princessbride/PrincessBride.java
@@ -1,11 +1,13 @@
-/*Canada!
+/*USA is A OK
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package princessbride;
 
+import byui.cit260.princessBride.model.Item;  //imported by Lai
 import byui.cit260.princessBride.model.Location;
+import byui.cit260.princessBride.model.Skills;
 
 /**
  *
@@ -18,13 +20,37 @@ public class PrincessBride {
      */
     public static void main(String[] args) {
        
-        Location locationOne= new Location();
+        Location locationOne= new Location(); 
         
         locationOne.setName("Florin Castle");
         locationOne.setBestTime(7.00);
         
         String locationInfo = locationOne.toString();
         System.out.println(locationInfo);
+        
+        Item itemOne = new Item();  //added by Lai
+        
+        itemOne.setName("Rock");
+        itemOne.setType("heavy");
+        itemOne.setPointRequired(40);
+        itemOne.setRowCount(53);
+        itemOne.setColumnCount(54);
+        
+        String intemInfo = itemOne.toString();
+        System.out.println(intemInfo);
+        
+        Skills skillsOne = new Skills();
+        
+        skillsOne.setTrueLove(70);
+        skillsOne.setFencing(70);
+        skillsOne.setArchery(60);
+        skillsOne.setCast(65);
+        skillsOne.setWisdom(90);
+        skillsOne.setIntrepidity(80);
+        skillsOne.setImmunity(85);
+        
+        String skillsInfo = skillsOne.toString();
+        System.out.println(skillsInfo);
     }
     
 }
