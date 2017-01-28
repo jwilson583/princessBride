224a56ab5ad9c9de5381cde2407e15f859ca2e7a
Ortega-Dan <ort13016@byui.edu>
2017-01-27 22:34:26 -0500

    Including assigned classes and adding them to the test. Successful test including the previous entered classes by Jack and Lai.

--- PrincessBride/src/princessbride/PrincessBride.java
+++ PrincessBride/src/princessbride/PrincessBride.java
@@ -1,12 +1,15 @@
-/*USA is A OK
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package princessbride;
 
+import byui.cit260.princessBride.model.BackPack;
+import byui.cit260.princessBride.model.Game;
 import byui.cit260.princessBride.model.Item;  //imported by Lai
 import byui.cit260.princessBride.model.Location;
+import byui.cit260.princessBride.model.Map;
 import byui.cit260.princessBride.model.Skills;
 
 /**
@@ -51,6 +54,38 @@ public class PrincessBride {
         
         String skillsInfo = skillsOne.toString();
         System.out.println(skillsInfo);
+        
+        
+        //Calling and testing classes included by Dan
+        
+        //Class 1
+        Game firstGame = new Game();
+        firstGame.setTotalTime(1.45);
+        firstGame.setNoPeople(4);
+        
+        String gameInfo = firstGame.toString();
+        System.out.println(gameInfo);
+   
+        //Class 2
+        Map hereTheMap = new Map();
+        hereTheMap.setColumnCount(5);
+        hereTheMap.setRowCount(18);
+        
+        String mapInfo = hereTheMap.toString();
+        System.out.println(mapInfo);
+        
+        //Class 3
+        BackPack myBack = new BackPack();
+        myBack.setPocket1("Knife");
+        myBack.setPocket2("Iocane Powder");
+        myBack.setPocket3("Mask");
+        myBack.setPocket4("Rubik's Cube");
+        
+        String backPackinfo = myBack.toString();
+        System.out.println(backPackinfo);
+        
+        
+        
     }
     
 }
