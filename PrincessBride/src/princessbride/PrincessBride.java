f2bf10a0bb937e65ce561fd0db4311888911fe74
Ortega-Dan <ort13016@byui.edu>
2017-01-28 20:51:38 -0500

    Just indenting and organizing PrincessBride class.

--- PrincessBride/src/princessbride/PrincessBride.java
+++ PrincessBride/src/princessbride/PrincessBride.java
@@ -5,9 +5,11 @@
  */
 package princessbride;
 
+//All imports required by the classes done by Lai, Jack, Ben and Dan.
+
 import byui.cit260.princessBride.model.BackPack;
 import byui.cit260.princessBride.model.Game;
-import byui.cit260.princessBride.model.Item;  //imported by Lai
+import byui.cit260.princessBride.model.Item;
 import byui.cit260.princessBride.model.Location;
 import byui.cit260.princessBride.model.Player;
 import byui.cit260.princessBride.model.Skills;
@@ -25,14 +27,8 @@ public class PrincessBride {
      */
     public static void main(String[] args) {      
        
-        Location locationOne = new Location(); 
-        locationOne.setName("Florin Castle");
-        locationOne.setBestTime(7.00);
-        
-        String locationInfo = locationOne.toString();
-        System.out.println(locationInfo);
-        
-        Item itemOne = new Item();  //added Item and Skills entries by Lai
+        //added Item and Skills entries by Lai
+        Item itemOne = new Item();  
         
         itemOne.setName("Rock");
         itemOne.setType("heavy");
@@ -56,8 +52,45 @@ public class PrincessBride {
         String skillsInfo = skillsOne.toString();
         System.out.println(skillsInfo);
         
-        // Testing classes by Jack 
+		//Calling and testing classes included by Dan
         
+        //Class 1
+        Game firstGame = new Game();
+        firstGame.setTotalTime(1.45);
+        firstGame.setNoPeople(4);
+        
+        String gameInfo = firstGame.toString();
+        System.out.println(gameInfo);
+   
+        //Class 2
+        Map hereTheMap = new Map();
+        hereTheMap.setColumnCount(5);
+        hereTheMap.setRowCount(18);
+        
+        String mapInfo = hereTheMap.toString();
+        System.out.println(mapInfo);
+        
+        //Class 3
+        BackPack myBack = new BackPack();
+        myBack.setPocket1("Knife");
+        myBack.setPocket2("Iocane Powder");
+        myBack.setPocket3("Mask");
+        myBack.setPocket4("Rubik's Cube");
+        
+        String backPackinfo = myBack.toString();
+        System.out.println(backPackinfo);
+
+
+
+        // Testing classes by Jack 
+		Location locationOne = new Location(); 
+        locationOne.setName("Florin Castle");
+        locationOne.setBestTime(7.00);
+        
+        String locationInfo = locationOne.toString();
+        System.out.println(locationInfo);
+
+
         Player playerOne = new Player();  
         
         playerOne.setName("Westley");
@@ -76,13 +109,37 @@ public class PrincessBride {
         System.out.println(storeInfo);
         
         Traps trapsOne = new Traps();  
-        
+
         trapsOne.setName("Lightening Quick Sand");
         trapsOne.setDamage(50);
         trapsOne.setDisarm(60);
               
         String trapsInfo = trapsOne.toString();
         System.out.println(trapsInfo);
+   
+        
+        //Classes by Ben
+        Scene sceneOne = new Scene();
+    
+        sceneOne.setDescription("The Great Forest");
+        sceneOne.setTravelTime(7.00);
+        sceneOne.setType("friendly");
+
+        String sceneInfo = sceneOne.toString();
+        System.out.println(sceneInfo);
+        
+        
+        NPC npcOne = new NPC();
+        npcOne.setName("Goblin");
+        npcOne.setHealth(10.0);
+        npcOne.setDefence(5.0);
+        npcOne.setSkill("sword");
+        npcOne.setDescription("Green and Scary");
+
+        String npcinfo = npcOne.toString();
+        System.out.println(npcinfo);
+
     }
+
     
 }
