74ed9efa512515e4749863e7b058aea32c94b8c3
Jack Wilson <Jack Wilson@192.168.2.9>
2017-03-15 21:24:24 -0400

    3/9/17- Jack Wilson- created a backpack control working on function

---
+++ PrincessBride/src/buyi/cit260/princessBride/control/BackPackControl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buyi.cit260.princessBride.control;
+
+import byui.cit260.princessBride.model.BackPack;
+
+/**
+ *
+ * @author Jack Wilson
+ */
+public class BackPackControl {
+
+  static BackPack createBackPack() {
+        BackPack backpack = new Backpack();
+        return backpack;
+    }
+    
+
+
+ public int sumCredits(int array[]){
+        int sum = 0;
+        for(int a: array){
+            sum += a;
+        }
+        return sum;
+    }
+
+    private static class Backpack extends BackPack {
+
+        public Backpack() {
+        }
+    }
+ 
+   
+}
